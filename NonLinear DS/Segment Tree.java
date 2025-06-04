c0ceef575c65af1d795b53dd65e38564199954c1
SusmithaTavva <162178028+SusmithaTavva@users.noreply.github.com>
2025-06-04 10:29:52 +0530

    Create Segment Tree.java

---
+++ NonLinear DS/Segment Tree.java
@@ -0,0 +1,2 @@
+*Segment Tree: in an array if we want to get the sum of particular range values we use a simple for loop, but the time complxity will be O(n).
+                 and we want to get the output in O(log n), So we have to use Segment tree or  binary search
