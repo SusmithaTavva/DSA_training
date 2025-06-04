dbadbe4a34349d18ca64c8256cee097b2444f812
SusmithaTavva <162178028+SusmithaTavva@users.noreply.github.com>
2025-06-04 10:24:49 +0530

    Create Binary Search Tree.java

---
+++ NonLinear DS/Binary Search Tree.java
@@ -0,0 +1,15 @@
+BinarySearchTree:-
+  the left value of the node should be lees than the root value and the root value of the node should be less than right node's value(left<Root<Right)
+Adv: for BST if we print inorder traversal we get the output in sorted
+Disadv: the tree might be skewed(all nodes have only child)
+
+*Balenced Tree: height(left tree) - height (right tree) == -1, 0 , 1
+
+*AVL : AVL tree is self balanced tree
+  4 types:left-left
+          right-right
+          left-right
+          right-left
+we use AVL tree to make a BST balanced in worst case
+
+ 
