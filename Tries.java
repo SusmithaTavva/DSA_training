0657318f0e59f89ff1a009f0a86656d3def55d43
SusmithaTavva <162178028+SusmithaTavva@users.noreply.github.com>
2025-06-05 09:02:56 +0530

    Create Tries.java

---
+++ Tries.java
@@ -0,0 +1,8 @@
+we have to create a node which can store a character array of 26 characters
+the last node need to change the flag to true
+  ADV:
+to get faster retrivals of the strings
+  class node{
+      int[26] data;
+      boolean flag;
+      Node next;
