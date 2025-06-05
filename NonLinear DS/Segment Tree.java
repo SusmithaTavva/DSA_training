7da366f0ac0e822b1de20cb94b67a1392f61250d
SusmithaTavva <162178028+SusmithaTavva@users.noreply.github.com>
2025-06-05 08:01:41 +0530

    Update Segment Tree.java

--- NonLinear DS/Segment Tree.java
+++ NonLinear DS/Segment Tree.java
@@ -1,2 +1,6 @@
 *Segment Tree: in an array if we want to get the sum of particular range values we use a simple for loop, but the time complxity will be O(n).
                  and we want to get the output in O(log n), So we have to use Segment tree or  binary search
+
+
+if given a range to find their sum in an array then if we use for loop the time complexity will be O(n^2)
+  so to reduce that we use segment trees in which we get O(log n)
